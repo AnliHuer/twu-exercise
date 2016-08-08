e81d900c5b6b2062564305233d04f790c20d7332
Anlihuer <anlihuer@outlook.com>
2016-08-08 10:33:08 +0800

    Trignale-Exercise

---
+++ Trignale-Exercise/src/DrawHorizontalLine.java
@@ -0,0 +1,13 @@
+public class DrawHorizontalLine {
+
+    public static void main(String[] args) {
+        new DrawHorizontalLine().printString(8);
+    }
+
+
+    public void printString(int n) {
+        for (int i = 0; i < n; i++) {
+            System.out.println("*");
+        }
+    }
+}
---
+++ Trignale-Exercise/src/DrawRightTriangle.java
@@ -0,0 +1,18 @@
+public class DrawRightTriangle {
+    public static void main(String[] args) {
+        new DrawRightTriangle().printString(8);
+    }
+
+    private void printString(int n) {
+
+        for (int i = 1; i <= n; i++) {
+            String str = "";
+
+            for (int j = 0; j < i; j++) {
+                str += "*";
+            }
+
+            System.out.println(str);
+        }
+    }
+}
